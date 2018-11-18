8cbfb3e42cdb3905d9443b1c96b5c7a7d410fe0d
Tatyana563 <nikolaev.english@gmail.com>
2018-11-18 19:11:17 +0200

    commit2

--- src/Group.java
+++ src/Group.java
@@ -1,10 +1,49 @@
-//import java.util.HashMap;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
-//public class Group {
- //   Map<String, Integer> hashmap = new HashMap< String, Integer>();
-//}
-//public class Student{
+public class Group {
+public class Student{
+public Student(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
 
-//}
+    @Override
+    public String toString() {
+        return "Student{" +
+                "stud=" + stud +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", students=" + students +
+                '}';
+    }
+
+    public  void main(String[] args) {
+        Student stud = new Student( "Tom",20);
+       stud.addStudents('Jack', 21);
+        System.out.println(students);
+    }
+
+
+    Map<Student, Integer> students = new HashMap();
+public  int averageage(String name,int age){
+
+    Set<Map.Entry<Student,Integer>> spisok=students.entrySet();
+        int totalSum=0;
+        for(Map.Entry<Student,Integer> temp:spisok){
+            totalSum=totalSum+temp.getValue()/students.size() ;
+        }
+
+
+        return totalSum;
+    }
+    public void addStudents(Student stud, int age){
+    students.put(stud,age);
+    }
+
+
+}
+}
+
